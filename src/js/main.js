6b0d9acea2600da5e658328e70453e8073ef01e7
hoangviethung <hoangviet.hung.009@gmail.com>
2020-03-03 13:42:02 +0700

    megre HOÀNG AND HÙNG

--- src/js/main.js
+++ src/js/main.js
@@ -257,160 +257,59 @@ const setHeightOverFolowBySomeElement = () => {
 	}
 }
 
-// SLIDER HERE !!!
-const homeSlider = () => {
-	var swiper = new Swiper('.main-page-slider .swiper-container', {
-		loop: true,
+function aboutMember() {
+	var swiper = new Swiper('.member-slide', {
+		centeredSlides: true,
+		slidesPerView: 3,
 		speed: 1000,
-		autoplay: {
-			delay: 3000,
-			disableOnInteraction: false,
-		},
-		grabCursor: true,
-		watchSlidesProgress: true,
-		mousewheelControl: true,
-		keyboardControl: true,
-		pagination: {
-			el: '.main-page-slider .swiper-pagination',
-			type: 'bullets',
-			clickable: true,
-		},
-		on: {
-			progress: function() {
-				var swiper = this;
-				for (var i = 0; i < swiper.slides.length; i++) {
-					var slideProgress = swiper.slides[i].progress;
-					var innerOffset = swiper.width * 0.5;
-					var innerTranslate = slideProgress * innerOffset;
-					swiper.slides[i].querySelector(".swiper-inner").style.transform =
-						"translate3d(" + innerTranslate + "px, 0, 0)";
-				}
-			},
-			touchStart: function() {
-				var swiper = this;
-				for (var i = 0; i < swiper.slides.length; i++) {
-					swiper.slides[i].style.transition = "";
-				}
-			},
-			setTransition: function(speed) {
-				var swiper = this;
-				for (var i = 0; i < swiper.slides.length; i++) {
-					swiper.slides[i].style.transition = speed + "ms";
-					swiper.slides[i].querySelector(".swiper-inner").style.transition =
-						speed + "ms";
-				}
-			}
-		}
-	})
-}
-
-// SLIDER SẢN PHẨM TRANG CHỦ
-const homeProductSlider = () => {
-	var swiper = new Swiper('.home-product .swiper-container', {
-		slidesPerView: 1,
-		spaceBetween: 20,
 		loop: true,
-		speed: 1000,
-		autoplay: {
-			delay: 3000,
-			disableOnInteraction: false,
-		},
+		autoplay: true,
 		navigation: {
-			nextEl: '.home-product .swiper-button-next',
-			prevEl: '.home-product .swiper-button-prev',
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
 		},
 		breakpoints: {
-			1440: {
-				slidesPerView: 4,
-			},
-			1100: {
-				slidesPerView: 3,
-			},
 			768: {
-				slidesPerView: 2,
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			375: {
+				slidesPerView: 1,
+				spaceBetween: 10,
 			}
 		},
-	})
-}
-
-// HEADER HERE !!!
-const activeHeaderWhenScroll = () => {
-	const heightHeader = document.querySelector('header').offsetHeight;
-	window.addEventListener('scroll', function() {
-		if (window.pageYOffset >= heightHeader) {
-			document.querySelector('header').classList.add('scrolled');
-		} else {
-			document.querySelector('header').classList.remove('scrolled');
-		}
-	})
-}
-
-// CHECK LAYOUT CÓ BANNER KHÔNG
-const checkLayoutBanner = () => {
-	const mainSlider = $('.main-page-slider');
-	const breadcrumb = $('#breadcrumb-wrapper');
-	const heightHeader = $('header').outerHeight();
-	console.log(heightHeader);
-	if (mainSlider.length >= 1) {
-		mainSlider.css('padding-top', heightHeader);
-	} else if (breadcrumb.length >= 1) {
-		breadcrumb.css('padding-top', heightHeader);
-	} else {
-		$('main').css('padding-top', heightHeader);
-	}
-}
-function aboutMember() {
-    var swiper = new Swiper('.member-slide', {
-        centeredSlides: true,
-        slidesPerView: 3,
-        speed: 1000,
-        loop: true,
-        autoplay: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-            375: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            }
-        },
-    });
+	});
 }
 
 function newBanner() {
-    var swiper = new Swiper('.new-slide, .hr, .download-slide', {
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'fraction',
-        },
-        centeredSlides: true,
-        speed: 1000,
-        loop: true,
-        spaceBetween: 0,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+	var swiper = new Swiper('.new-slide, .hr, .download-slide', {
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'fraction',
+		},
+		centeredSlides: true,
+		speed: 1000,
+		loop: true,
+		spaceBetween: 0,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 }
+
 function ajaxForm() {
 	$('form button').on('click', function(e) {
 		e.preventDefault();
 		const url = $(this).attr('data-url');
 		const name = $('#name').val();
 		const phone = $('#phone').val();
-        const email = $('#email').val();
-        const noidung = $('#noidung').val();
+		const email = $('#email').val();
+		const noidung = $('#noidung').val();
 		$.ajax({
 			type: "POST",
 			url: url,
@@ -418,8 +317,8 @@ function ajaxForm() {
 				url: url,
 				name: name,
 				phone: phone,
-                email: email,
-                noidung: noidung
+				email: email,
+				noidung: noidung
 			},
 			success: function(res) {
 				if (res.Code === 200) {
@@ -431,16 +330,22 @@ function ajaxForm() {
 		});
 	});
 }
+
 function responText() {
-    $(window).resize(function() {
-        var width = $(window).width();
-        if (width < 658) {
-            $('.nb-1, .nb-4').find('.text-content').css({ display: "none" });
-        } else {
-            $('.nb-1, .nb-4').find('.text-content').css({ display: "block" });
-        }
-    });
+	$(window).resize(function() {
+		var width = $(window).width();
+		if (width < 658) {
+			$('.nb-1, .nb-4').find('.text-content').css({
+				display: "none"
+			});
+		} else {
+			$('.nb-1, .nb-4').find('.text-content').css({
+				display: "block"
+			});
+		}
+	});
 }
+
 function showBackToTop() {
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 0) {
@@ -482,6 +387,13 @@ document.addEventListener('DOMContentLoaded', () => {
 	// TAB
 	const tabInformationDetail = new Tab('.block-tab-information-detail');
 	// GET HEIGHT SOMWE ELEMENT
+
+	// HOÀNG JS
+	aboutMember();
+	newBanner();
+	ajaxForm();
+	responText();
+	showBackToTop();
 });
 
 window.addEventListener("resize", () => {
